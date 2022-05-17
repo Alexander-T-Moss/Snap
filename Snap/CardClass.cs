966071cf8b0b8b75e7eca608195eb4b548a3575c
Alexander Þór <alexander.thorunnarson@gmail.com>
2022-05-17 14:59:43 +0100

    Initial Backup
    Not the finished version as still quite deep in the pit of development :)

---
+++ Snap/CardClass.cs
@@ -0,0 +1,25 @@
+﻿namespace Snap
+{
+	public class Card
+	{
+        protected int cardNumber;
+        protected string cardSuit;
+        
+		public Card(int number, string suit)
+		{
+            cardNumber = number;
+            cardSuit = suit;
+ 		}
+
+        public string GetSuit()
+        {
+            return cardSuit;
+        }
+
+        public int GetNumber()
+        {
+            return cardNumber;
+        }
+	}
+}
+
---
+++ Snap/QueueClass.cs
@@ -0,0 +1,52 @@
+﻿namespace Snap
+{
+    public class Queue
+	{
+        private List<Card> queue;
+
+        public Queue()
+		{
+            this.queue = new();
+		}
+
+        public bool Empty()
+        {
+            if (queue.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Card? First()
+        {
+            if (Empty())
+            {
+                return null;
+            }
+            else
+            {
+                return queue[0];
+            }
+        }
+
+        public int Size()
+        {
+            return queue.Count;
+        }
+
+        public void Enqueue(Card card)
+        {
+            queue.Add(card);
+        }
+
+        public void Dequeue()
+        {
+            queue.RemoveAt(0);
+        }
+	}
+}
+
